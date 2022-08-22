ca67e5e6dd5bc21ad1a9c37d1c4d521cd0da3b09
Eray Mutlu <75501882+KanNudimmud@users.noreply.github.com>
2022-08-22 13:17:32 +0300

    Add files via upload

--- diabetes_web_application/init.R
+++ diabetes_web_application/init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("e1071", "data.table","shiny","caTools","caret")
+my_packages = c("e1071", "data.table","shiny","caTools","caret","markdown")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
